92aee351386c48c4728ac6c9292bea0c830d0eb4
Dzung Nguyen <dungtoanhoc@yahoo.com.vn>
2016-06-09 13:43:24 +0700

    Add some features

--- js/header.js
+++ js/header.js
@@ -1,40 +1,65 @@
 $(document).ready(function() {
-    $('.dropdown > a').click(function() {
+    $(window).click(function() {
+        $('#left-menu').hide('slide', { direction: 'left' });
+        $('#add-to-cart, #checkout').hide('slide', { direction: 'right' });
+        $('.button .dropup ul').hide();
+    });
+
+    $('#left-menu, #add-to-cart, #checkout').click(function() {
+        event.stopPropagation();
+    });
+
+    // Menu dropdown
+    $('.menu-dropdown > a').click(function() {
         $(this).siblings('.sub-menu').slideToggle();
         $(this).parent().toggleClass('active');
 
-        var text = $(this).text();
-        if (text[0] === '▼')
-            $(this).text(text.substr(1));
+        var text = $(this).html();
+        if (text[0] === '<')
+            $(this).text(text.substr(33));
         else
-            $(this).prepend('&#9660; ');
+            $(this).prepend('<i class="fa fa-caret-down"></i> ');
     });
 
+    // Menu
     $('#button-menu').click(function() {
-        $('#left-menu').slideToggle();
+        event.stopPropagation();
+        $('#left-menu').toggle('slide', { direction: 'left' });
     });
 
+
+    // Count, add-to-cart and checkout
     $('#count').click(function() {
-        $('#add-to-cart').slideToggle('slow');
+        event.stopPropagation();
+        $('#add-to-cart').toggle('slide', { direction: 'right' });
+        $('#left-menu').hide('slide', { direction: 'left' });
         $('#checkout').hide();
     });
 
     $('#add-to-cart .button-add-to-cart').click(function() {
-        $('#checkout').slideToggle();
-        $('#add-to-cart').toggle();
+        $('#checkout').toggle('slide', { direction: 'right' });
+        $('#add-to-cart').hide();
     });
 
-    $('#button-checkout').click(function() {
-        $('#checkout').slideToggle();
+    $('#add-to-cart').click(function() {
+        $('#add-to-cart .dropup ul').hide();
     });
 
+    // Close item
     $('.close').click(function() {
         $(this).parent().toggle();
     });
 
+    // click the little arrow to scroll
     $('.switch').click(function() {
         $('body, html').animate({
             scrollTop: $('#list').offset().top - 60
         }, 900);
+    });
+
+    // color button
+    $('.dropup button').click(function() {
+        event.stopPropagation();
+        $(this).siblings('ul').slideToggle();
     });
 });
